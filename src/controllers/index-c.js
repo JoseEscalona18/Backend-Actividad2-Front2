660af95088f204b96938375c034c08053374cadf
JoseEscalona18 <jose.al.es301@gmail.com>
2023-10-20 00:43:24 -0400

    Condiciones agregar

--- src/controllers/index-c.js
+++ src/controllers/index-c.js
@@ -23,19 +23,39 @@ class productosController {
     try {
 
         const {serial, nombre, descripcion, cantidad, precio, imagen, categoria} = req.body;
+        const serialEquipos = JSON.stringify(await products.find({serial: serial}))
+        if (serialEquipos == '[]'){
+            const nuevoproducto = new products({serial, nombre, descripcion, cantidad, precio, categoria, imagen});
+            await nuevoproducto.save()
+            res.status(201).send('Producto agregado correctamente');
 
-        console.log({serial, nombre, descripcion, cantidad, precio, imagen, categoria})
-
-        const nuevoproducto = new products({serial, nombre, descripcion, cantidad, precio, categoria, imagen});
-        await nuevoproducto.save()
-
-        res.status(200).send('Producto agregado correctamente');
-
+        }else{
+            res.status(400).send('Serial de equipo ya registrado');
+        }
 
     } catch (error) {
       res.status(404).json({Error: 'Error al obtener productos'})
     }
   };
+
+  busquedaProductos = async (req, res) => {
+    try {
+
+        const serial = req.params.serial;
+        const productos = await products.findById(serial);
+
+
+        if (productos == ''){
+            res.status(200).send('No hay productos en la Base de Datos');
+        } else {
+            res.status(200).send(productos);
+        }
+
+    } catch (error) {
+      res.status(404).json({Error: 'Error al obtener productos'})
+    }
+  };
+
 
 }
 
